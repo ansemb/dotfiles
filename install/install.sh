afc7be19306147dfc51adfa2deeee93559679b5e
Adrian Nadau Semb <31008843+ansemb@users.noreply.github.com>
2021-02-22 18:21:59 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -23,6 +23,14 @@ export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
 export PATH="$PATH:$HOME/.linuxbrew"
 brew install gcc exa
 
+# nvm install
+export NVM_DIR="$HOME/.config/nvm"
+mkdir $NVM_DIR
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/HEAD/install.sh NVM_DIR="$HOME/.config/nvm" | bash
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# install node
+nvm install node
 
 # get home directory of user executing script
 home_dir=$HOME
