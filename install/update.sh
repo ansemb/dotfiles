9e5bd0fddd10a9b33b4ffe6e84d40fb0b505eabc
ansemb <31008843+ansemb@users.noreply.github.com>
2021-04-28 15:52:18 +0200

    allow enter instead of explicit y

--- install/update.sh
+++ install/update.sh
@@ -14,14 +14,14 @@ shopt -s expand_aliases
 alias dotfiles='/usr/bin/git --git-dir=$dotfiles_dir/ --work-tree=$home_dir'
 
 dotfiles fetch
-if [ $(dotfiles log -p HEAD..FETCH_HEAD | wc -l) -eq 0 ]; then
+if [[ $(dotfiles log -p HEAD..FETCH_HEAD | wc -l) -eq 0 ]]; then
 	echo "No changes. Exiting..."
 	exit
 fi
 
 read -p "Changes detected on remote version. Continue update (this will overwrite local changes) (Y/n)? " -n 2 -r
 echo ""
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+if [[ ! $REPLY =~ ^[Yy]$ ]] || [[ -z "$REPLY" ]]; then
 	exit
 fi
 
@@ -31,7 +31,7 @@ dotfiles config --local status.showUntrackedFiles no
 # ignore readme
 dotfiles update-index --assume-unchanged "$home_dir/README.md"
 
-if [ -f "$home_dir/README.mb" ]; then
+if [[ -f "$home_dir/README.mb" ]]; then
 	rm "$home_dir/README.md"
 fi
 
@@ -45,11 +45,11 @@ done
 
 
 # do cleanup
-if [ -d "$home_dir/install" ]; then
+if [[ -d "$home_dir/install" ]]; then
 	# prompt user to remove install directory; user might have files in this directory
 	read -p "Remove directory '$home_dir/install' completely (used for install files, but if directory is already used by you, answer no) (Y/n)? " -n 1 -r
 	echo ""
-	if [[ $REPLY =~ ^[Yy]$ ]] || [ -z $REPLY ]; then
+	if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
 		echo "Removing '$home_dir/install'"
 		rm -rf "$home_dir/install"
 	fi
