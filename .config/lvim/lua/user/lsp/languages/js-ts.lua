e3e5c49b778519c2d2a5486cc3c66e1361c835cd
ansemb <31008843+ansemb@users.noreply.github.com>
2022-10-24 12:18:23 +0200

    updated

--- .config/lvim/lua/user/lsp/languages/js-ts.lua
+++ .config/lvim/lua/user/lsp/languages/js-ts.lua
@@ -31,12 +31,12 @@ formatters.setup {
 }
 
 local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
--- require("nvim-dap-vscode-js").setup {
---   -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
---   debugger_path = mason_path .. "packages/js-debug-adapter", -- Path to vscode-js-debug installation.
---   -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
---   adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
--- }
+require("dap-vscode-js").setup {
+  -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+  debugger_path = mason_path .. "packages/js-debug-adapter", -- Path to vscode-js-debug installation.
+  -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+  adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+}
 
 for _, language in ipairs { "typescript", "javascript" } do
   require("dap").configurations[language] = {
