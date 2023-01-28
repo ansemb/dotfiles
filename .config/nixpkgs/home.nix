aaae72c290dd67f1d558a87378a6fb28752ff97d
ansemb <31008843+ansemb@users.noreply.github.com>
2023-01-28 22:12:25 +0100

    initial nix

---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,19 @@
+{ pkgs, ...}: {
+  home.username = "adrian";
+  home.homeDirectory = "/home/adrian";
+
+  # Packages
+  home.packages = [ 
+    pkgs.fish
+    pkgs.zsh
+    pkgs.exa
+    pkgs.helix
+  ];
+
+
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
+
