7934816deff3b16083878c901fb3a6af7152f598
Adrian Nadau Semb <31008843+ansemb@users.noreply.github.com>
2025-09-10 07:51:54 +0000

    update install

--- install/install.zsh
+++ install/install.zsh
@@ -7,10 +7,36 @@ fi
 
 OS="$(uname)"
 
-# source common
-source <(curl -fsSL https://raw.githubusercontent.com/ansemb/dotfiles/HEAD/.config/zsh/common.zsh)
+
+# VARIABLES
+export CONFIG_HOME="$HOME/.config"
+export CACHEDIR="$HOME/.cache"
+
+
+# DOTFILELS
+export DOTFILES_DIR="$HOME/.dotfiles"
+export DOTFILES_REPO="https://raw.githubusercontent.com/ansemb/dotfiles/HEAD"
+
 
 # PATHS
+
+function pathappend() {
+  for ARG in "$@"; do
+    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+       PATH="${PATH:+"$PATH:"}$ARG"
+    fi
+  done
+}
+
+function pathprepend() {
+  for ARG in "$@"; do
+    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+        PATH="$ARG${PATH:+":$PATH"}"
+    fi
+  done
+}
+
+
 mkdir -p "$HOME/.local/bin"
 pathappend "$HOME/.local/bin"
 
@@ -228,6 +254,12 @@ function clean_prev_install_dotfiles() {
   fi
 }
 
+
+# dotfiles
+function dotfiles {
+  /usr/bin/git --git-dir="$DOTFILES_DIR/" --work-tree="$HOME" "$@"
+}
+
 function dotfiles_install() {
   echo "Installing dotfiles into dir: $HOME"
   if [ ! -f "$HOME/.gitignore" ] || ! grep -q ".dotfiles" "$HOME/.gitignore"; then
@@ -240,6 +272,37 @@ function dotfiles_install() {
   dotfiles config --local status.showUntrackedFiles no
 }
 
+
+function dotfiles_update_index() {
+  # remove unwanted files and update
+  # git index
+
+  # ignore and remove readme
+  readme_file="$HOME/README.md"
+  dotfiles update-index --assume-unchanged "$readme_file"
+  if [[ -f "$readme_file" ]]; then
+    rm "$readme_file"
+  fi
+
+  # ignore and remove install directory files
+  while IFS= read -r filename; do
+    echo "dotfiles, ignoring file: $filename"
+    dotfiles update-index --assume-unchanged "$filename"
+    if [[ -f "$HOME/$filename" ]]; then
+      echo "dotfiles, removing file: $filename"
+      rm "$HOME/$filename"
+    fi
+  done < <(dotfiles ls-files "$HOME/install")
+
+  # do cleanup
+  if [[ -d "$HOME/install" ]]; then
+    echo "dotfiles, removing install directory if empty."
+    rmdir "$HOME/install" >/dev/null 2>&1
+  fi
+
+  # let user settings be changed without tracking
+  dotfiles update-index --skip-worktree "$ZDOTDIR/user-settings.zsh"
+}
 
 #########################################
 # INSTALLATION
@@ -266,7 +329,7 @@ clean_prev_install_dotfiles
 dotfiles_install
 
 # cleanup
-dotfiles-update-index
+dotfiles_update_index
 
 # run zsh shell to download zsh plugins
 /bin/zsh -i -c exit
