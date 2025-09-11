9e52372c0e9eae5e2878b5b1d21b2dffb826a774
Adrian Nadau Semb <31008843+ansemb@users.noreply.github.com>
2025-09-11 06:58:40 +0000

    update fnm completion

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,4 +1,7 @@
 if type -q fnm
   fnm env --use-on-cd --shell fish | source
-  fnm completions --shell fish
+  set fnm_completions_file "$FISH_HOME/completions/fnm.fish"
+  if not test -f "$fnm_completions_file"
+    fnm completions --shell fish > "$fnm_completions_file"
+  end
 end
